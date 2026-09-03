7148c8671887687499d813e57d41a83d5ae28c52
Mysteriousgu <119445080+Mysteriousgu@users.noreply.github.com>
2026-09-03 09:59:01 +0700

    Add files via upload

---
+++ Program1_2611532008.py
@@ -0,0 +1,20 @@
+# Program pertama
+"""
+Komentar
+Dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print ("Hello world")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print(" / \\ // \\\\ /// \\\\\ ")
+print ("Program ini " "Dibuat oleh a\n"
+       "Muhammad Rafi Firdaus 2611532008")
+echo "# 2026_Alpro_B_2611532008" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Mysteriousgu/2026_Alpro_B_2611532008.git
+git push -u origin main
